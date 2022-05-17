1a178cf0ffc3adf6cc6eb70bf825f6b89f746f0d
unijreng <redomeire2@gmail.com>
2022-05-17 08:07:36 +0700

    adding template parameter to_name

--- Home/Home.js
+++ Home/Home.js
@@ -38,15 +38,19 @@ form.addEventListener("submit", (e) => {
 
 const fetchingData = () => {
     let template_params = {
+        to_name: "Wateers.io",
         from_name: document.querySelector(".form-input-name").value,
         from_email: document.querySelector(".form-input-email").value,
         message: document.querySelector(".form-input-textarea").value
     }
+    
     emailjs.send('service_b1jo2ig', 'template_uwih5iy', template_params)
     .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
+        alert('Success sending email!!');
      }, function(error) {
         console.log('FAILED...', error);
+        alert("Sending email failed!");
      });
 }
 
