864a5e37636d4ab03297518d475c484b5739f74b
unijreng <redomeire2@gmail.com>
2022-05-17 23:39:08 +0700

    resolving nav link bugs

--- RegistrationPage/Registration.js
+++ RegistrationPage/Registration.js
@@ -18,13 +18,11 @@ window.addEventListener("scroll", () => {
         nav.style.backgroundColor = "#1687A7";
         anchors[0].style.color = "white";
         anchors[1].style.color = "white";
-        anchors[2].style.color = "white";
     }
     else {
         nav.style.backgroundColor = "transparent";
         anchors[0].style.color = "black";
         anchors[1].style.color = "black";
-        anchors[2].style.color = "black";
     }
 })
 
