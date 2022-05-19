61068a27f2f52b81d59ab7bd8c8561474a06fd22
unijreng <redomeire2@gmail.com>
2022-05-19 22:02:54 +0700

    separating component

--- RegistrationPage/Registration.js
+++ RegistrationPage/Registration.js
@@ -3,29 +3,6 @@
     emailjs.init("w4VKdzMIzXS3aP1Dl");
 })();
 
-//nav handling
-const nav = document.querySelector(".navbar");
-const navItems = document.querySelector(".nav-items");
-const anchors = document.getElementsByClassName("nav-link");
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", function(){
-    navItems.classList.toggle("slide");
-})
-
-window.addEventListener("scroll", () => {
-    if(window.scrollY > 100) {
-        nav.style.backgroundColor = "#1687A7";
-        anchors[0].style.color = "white";
-        anchors[1].style.color = "white";
-    }
-    else {
-        nav.style.backgroundColor = "transparent";
-        anchors[0].style.color = "black";
-        anchors[1].style.color = "black";
-    }
-})
-
 //form handling
 const form = document.querySelector(".jumbotron-form form");
 const scriptURL = "https://script.google.com/macros/s/AKfycbxmdt5FOfW-2yW3sKl2kthThwplq-fAtzAMrjnfyrsruPFEI2-bYrWHta03jlrLHflw3A/exec";
