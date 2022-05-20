3d3f358126f5bd503b4344b195fbd89ce5ff5145
unijreng <redomeire2@gmail.com>
2022-05-20 11:13:11 +0700

    fixing navbar colors

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,22 +3,33 @@ const nav = document.querySelector(".navbar");
 const navItems = document.querySelector(".nav-items");
 const anchors = document.getElementsByClassName("nav-link");
 const hamburger = document.querySelector(".hamburger");
+const hamburgerDiv = document.getElementsByClassName("hamburgerDiv");
 
 hamburger.addEventListener("click", function(){
     navItems.classList.toggle("slide");
 })
 
+if(window.scrollY <= 100) {
+    nav.style.backgroundColor = "transparent";
+    for(let i = 0; i < 3; i++){
+        anchors[i].style.color = "black";
+        hamburgerDiv[i].style.backgroundColor = "black"
+    }
+}
+
 window.addEventListener("scroll", () => {
     if(window.scrollY > 100) {
         nav.style.backgroundColor = "#1687A7";
-        anchors[0].style.color = "white";
-        anchors[1].style.color = "white";
-        anchors[2].style.color = "white";
+        for(let i = 0; i < 3; i++){
+            anchors[i].style.color = "white";
+            hamburgerDiv[i].style.backgroundColor = "white";
+        }
     }
     else {
         nav.style.backgroundColor = "transparent";
-        anchors[0].style.color = "black";
-        anchors[1].style.color = "black";
-        anchors[2].style.color = "black";
+        for(let i = 0; i < 3; i++){
+            anchors[i].style.color = "black";
+            hamburgerDiv[i].style.backgroundColor = "black";
+        }
     }
 })
